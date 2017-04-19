285dae4bba50be25e91291dc86a7c1b1619a2be9
Jimmy Soller <james.soller@gmail.com>
2017-04-19 15:06:48 -0400

    Update functions.php

--- functions.php
+++ functions.php
@@ -1,7 +1,28 @@
-//Example popular-posts/
+<?php
+
+
+//register sidebar widget area 
 
 
 
+function example_widgets_init() {
+	register_sidebar( array(
+		'name'          => esc_html__( 'Sidebar', 'acs-axial' ),
+		'id'            => 'sidebar-1',
+		'description'   => '',
+		'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+		'after_widget'  => '</aside>',
+		'before_title'  => '<h1 class="widget-title">',
+		'after_title'   => '</h1>',
+	) );
+}
+add_action( 'widgets_init', 'exampe_widgets_init' )
+
+
+
+
+
+//function to display popular post
 function example_popular_posts($post_id) {
 	$count_key = 'popular_posts';
 	$count = get_post_meta($post_id, $count_key, true);
@@ -14,3 +35,11 @@ function example_popular_posts($post_id) {
 		update_post_meta($post_id, $count_key, $count);
 	}
 }
+
+
+
+
+
+
+
+
