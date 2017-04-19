9259e8dd716c623277a763de4d12e3b0d47acdfa
Jimmy Soller <james.soller@gmail.com>
2017-04-19 14:33:23 -0400

    Create functions.php

---
+++ functions.php
@@ -0,0 +1,16 @@
+//Example popular-posts/
+
+
+
+function example_popular_posts($post_id) {
+	$count_key = 'popular_posts';
+	$count = get_post_meta($post_id, $count_key, true);
+	if ($count == '') {
+		$count = 0;
+		delete_post_meta($post_id, $count_key);
+		add_post_meta($post_id, $count_key, '0');
+	} else {
+		$count++;
+		update_post_meta($post_id, $count_key, $count);
+	}
+}
